97b089737cc5b4e88a12645ce7ce88ace064a95a
Noah Schutte <noahschutte@gmail.com>
2018-02-20 20:53:29 -0700

    log port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { createServer } = require('http');
 const next = require('next');
 const port = process.env.PORT || 3000;
 
+console.log('port: ', port);
+
 const app = next({
 	dev: process.env.NODE_ENV !== 'production',
 });
